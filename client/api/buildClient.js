ac08a77aad635635fa34e464487d31c57a96ba6f
Shien Lee <shienlee73@gmail.com>
2023-09-24 15:34:34 +0800

    fix wrong baseURL

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,14 +4,13 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "https://shienlee73.app",
       headers: req.headers,
     });
   } else {
     // client
     return axios.create({
-      baseURL: "https://shienlee73.app/",
+      baseURL: "/",
     });
   }
 };
